a369629f5026b163cd23f24112a6ced1674a375b
hanchon <guillermo.paoletti@gmail.com>
2023-06-13 22:14:22 +0200

    feat: autoregister users

--- internal/backend/messages/connect.go
+++ internal/backend/messages/connect.go
@@ -1,10 +1,14 @@
 package messages
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strings"
+	"time"
 
+	"github.com/bocha-io/garnet/internal/backend/api"
 	"github.com/bocha-io/garnet/internal/database"
 	"github.com/bocha-io/garnet/internal/logger"
 )
@@ -17,6 +21,39 @@ func connectMessage(ws *WebSocketContainer, usersDB *database.InMemoryDatabase, 
 	}
 
 	user, err := usersDB.Login(connectMsg.User, connectMsg.Password)
+	if err != nil {
+		// TODO: this is only used for the demo, so it autoregisters each user
+		client := &http.Client{
+			Timeout: 2 * time.Second,
+		}
+
+		body := api.RegistationParams{
+			Username: connectMsg.User,
+			Password: connectMsg.Password,
+		}
+
+		bodyBytes, err := json.Marshal(body)
+		if err != nil {
+			return err
+		}
+		r, err := http.NewRequest("POST", fmt.Sprintf("http://localhost:%d/signup", 6666), bytes.NewBuffer(bodyBytes))
+		if err != nil {
+			return err
+		}
+		r.Header.Add("Content-Type", "application/json")
+
+		res, err := client.Do(r)
+		if err != nil {
+			return fmt.Errorf("error sending the signup request: %s", err.Error())
+		}
+		if res.StatusCode != http.StatusOK {
+			return fmt.Errorf("incorrect response: %d", r.Response.StatusCode)
+		}
+
+		// return fmt.Errorf("incorrect credentials")
+	}
+
+	user, err = usersDB.Login(connectMsg.User, connectMsg.Password)
 	if err != nil {
 		return fmt.Errorf("incorrect credentials")
 	}
